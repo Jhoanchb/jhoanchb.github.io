8981366b38d536e69d55b6bfde2af209ef7fcb4b
Jhoan Chuecos <37130888+Jhoanchb@users.noreply.github.com>
2018-04-08 10:59:39 -0400

    Update details.js

--- js/details.js
+++ js/details.js
@@ -6,6 +6,10 @@ $('#skills').on('click', function(){
   $('.skillsopen').slideToggle(800);
 });
 
+$('#projects').on('click', function(){
+  $('.projectsopen').slideToggle(800);
+});
+
 $('#themes').on('click', function(){
   $('.themes').slideToggle(800);
 });
@@ -21,36 +25,3 @@ $('#red').on('click', function(){
 $('#blue').on('click', function(){
   $('.card').toggleClass('bluecard', 500);
 });
-
-
-
-/*$('.about-toggle').click(function() {
-  $('body').toggleClass('aboutcolor');
-  $('.aboutopen').toggleClass('aboutclose', 500);
-  $('.aboutcard').toggleClass('bgblanco', 500);
-  $('.aboutline, .aboutline::after').toggleClass('bgnegro', 500); 
-  $(this).toggleClass('open');  
-});
-
-$('.skills-toggle').click(function() {
-  $('body').toggleClass('skillscolor');
-  $('.skillsopen').toggleClass('skillsclose', 500);
-  $('.skillscard').toggleClass('bgblanco', 500);
-  $('.skillsline, .skillsline::after').toggleClass('bgnegro', 500); 
-  $(this).toggleClass('open');  
-});
-
-$('.contact-toggle').click(function() {
-  $('body').toggleClass('contactcolor');
-  $('.contactopen').toggleClass('contactclose', 500);
-  $('.contactcard').toggleClass('bgblanco', 500);
-  $('.contactline, .contactline::after').toggleClass('bgnegro', 500); 
-  $(this).toggleClass('open');  
-});
-
-$(window).scroll(function() {
-  if ($(document).scrollTop() > 60) {
-    $('header').addClass('navshow');} 
-  else {
-    $('header').removeClass('navshow');}
-  }) */
