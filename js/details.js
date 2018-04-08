0455bfe06ba1de009e8d3a6f8298c6c2c502d18d
Jhoan Chuecos <37130888+Jhoanchb@users.noreply.github.com>
2018-04-08 16:45:27 -0400

    Update details.js

--- js/details.js
+++ js/details.js
@@ -9,19 +9,3 @@ $('#skills').on('click', function(){
 $('#projects').on('click', function(){
   $('.projectsopen').slideToggle(800);
 });
-
-$('#themes').on('click', function(){
-  $('.themes').slideToggle(800);
-});
-
-$('#white, #black').on('click', function(){
-  $('body').toggleClass('whitetheme', 500);
-});
-
-$('#red').on('click', function(){
-  $('.card').toggleClass('redcard', 500);
-});
-
-$('#blue').on('click', function(){
-  $('.card').toggleClass('bluecard', 500);
-});
